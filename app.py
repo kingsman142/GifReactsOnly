55533abf792f852273b8233ea2d6382021803236
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 23:02:07 -0400

    update

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def my_test_endpoint():
     input_json = request.get_json(force=True) 
     # force=True, above, is necessary if another developer 
     # forgot to set the MIME type to 'application/json'
-    print 'data from client:', input_json
+    print('data from client:', input_json)
     dictToReturn = {'answer':42}
     return jsonify(dictToReturn)
 
