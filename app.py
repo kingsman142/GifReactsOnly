2350c2a881dec4d475ef2da2b87c849747fbbc03
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 21:07:01 -0400

    ??

--- app.py
+++ app.py
@@ -11,7 +11,16 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_files():
+    resp = flask.make_response()
+    if authenticate_user(request.form):
+        request.files['image'].save('path/to/file.jpg')
+        resp.status_code = 204
+    else:
+        resp.status_code = 411
+    return resp
+'''
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -34,6 +43,8 @@ def upload_file():
          <input type=submit value=Upload>
     </form>
     '''
+'''
+
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     return """
