17b03ee171b1f0c7433c299afbc4741ce313352d
Daniel Zheng <dzheng256@gmail.com>
2017-09-17 00:56:20 -0400

    finally work?

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ import json
 app = Flask(__name__)
 
 def get_stuff(query):
+    print(query)
     return requests.get('http://d.yimg.com/aq/autoc?query=' + query + '&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks').text
 
 
 @app.route('/tests/endpoint', methods=['GET', 'POST'])
 def my_endpoint():
-#x = get_stuff(json_data['company'])
-#ans = {'answer': json.loads(x[x.find('{'):-2])}
-    ans = {'answer': 'haha'}
+    
+    json_data = request.get_json(force=True)
+    print(json_data)
+    x = get_stuff(json_data['company'])
+    ans = {'answer': json.loads(x[x.find('{'):-2])}
     return jsonify(ans)
 
 if __name__ == '__main__':
