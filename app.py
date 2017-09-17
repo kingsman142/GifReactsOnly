fa90d07766d64df2607bc46bd75c8bce35b19845
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 23:03:30 -0400

    oops

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import os
 import logging
+from flask import Flask, render_template, request, url_for, jsonify
 #from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'pix'
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
-from flask import Flask, render_template, request, url_for, jsonify
-app = Flask(__name__)
 
 @app.route('/tests/endpoint', methods=['POST'])
 def my_test_endpoint():
