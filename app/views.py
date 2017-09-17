96b98cb679bbc1f7c12de383afe8ec7767b0f987
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 22:00:35 -0400

    update to views

--- app/views.py
+++ app/views.py
@@ -36,3 +36,6 @@ from flask import send_from_directory
     def uploaded_file(filename):
         return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
