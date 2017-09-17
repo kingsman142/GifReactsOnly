c3c32f1a4a4163d2dc5ec89af36fe7a75b1792ec
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 21:03:28 -0400

    fixed app

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
         return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/image", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
