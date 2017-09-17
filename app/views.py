65cbf862fc593dd79274c0fafa60781211ec897c
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 22:04:25 -0400

    update to view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from app import app
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -32,9 +39,9 @@ def upload_file():
     </form>
     '''
 from flask import send_from_directory
-    @app.route('/uploads/<filename>')
-    def uploaded_file(filename):
-        return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 
 if __name__ == "__main__":
