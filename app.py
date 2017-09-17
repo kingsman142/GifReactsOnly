7f8cc1c5f6ff1af32943c6ad8b199a5a78e28701
Daniel Zheng <dzheng256@gmail.com>
2017-09-17 09:02:10 -0400

    added links to financial articles

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import os
+from bs4 import BeautifulSoup
+import calendar
 import logging
 from flask import Flask, render_template, request, url_for, jsonify
 import requests
 import json
 import nasdaq
+from datetime import datetime, timedelta
 app = Flask(__name__)
 
-
-def get_stuff(query):
-    query = '+'.join(query.split())
-    return requests.get('http://d.yimg.com/aq/autoc?query=' + query + '&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks').text
+top_dates = {}
+def get_symbol(query): query = '+'.join(query.split()) return requests.get('http://d.yimg.com/aq/autoc?query=' + query + '&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks').text
 
 
 @app.route('/getDates', methods=['GET', 'POST'])
 def my_endpoint():
-    
     json_data = request.get_json(force=True)
     print(json_data)
 
-    x = get_stuff(json_data['company'])
-    ans = {'answer': json.loads(x[x.find('{'):-2])}
+    x = get_symbol(json_data['company'])
     results = json.loads(x[x.find('{'):-2])['ResultSet']['Result']
     code = None 
     for ans in results:
@@ -32,11 +31,48 @@ def my_endpoint():
         n.get_prices()
         dates_sliding = [(abs(n.prices[i]['Close']-n.prices[i-1]['Close']),n.prices[i]['DateStamp'][:n.prices[i]['DateStamp'].find('T')]) for i in range(1, len(n.prices))]
         best_dates = sorted(dates_sliding, key=lambda x:x[0])
-        top_dates = {'dates': sorted([x[1] for x in best_dates[-5:]])}
-        
+        top_dates['dates'] = sorted([x[1] for x in best_dates[-5:]])
+        top_dates['symbol'] = code
+        print(top_dates)
+        for s in top_dates['dates']:
+            date = datetime.strptime(s, "%Y-%m-%d")
+            modified_date = date + timedelta(days=1)
+            end = datetime.strftime(modified_date, "%Y-%m-%d")
+            start = s 
+            code = 'MSFT'
+            url = 'https://finance.google.com/finance/company_news?q=NASDAQ%3A{}&start=0&num=5&startdate={}&enddate={}'.format(code, start, end)
+            print(url)
+            page = requests.get(url)
+            soup = BeautifulSoup(page.text, 'html5lib')
+            articles = soup.find_all("a", {"id":"n-cn-"}) 
+            link_title = [(x.attrs['href'], x.text) for x in articles]
+            print(link_title)
+
+       
         return jsonify(top_dates)
     else:
-        return jsonify({})
+        return jsonify({'dates':[]})
+
+@app.route('/getGifs', methods=['GET', 'POST'])
+def gif_endpoint():
+    json_data = request.get_json(force=True)
+    date = json_data['date']
+    end = date[:-2] + str(int(date[-2:]))
+    start = date[:-2] + str(int(date[-2:]))
+    code = json_data['symbol']
+    url = 'https://finance.google.com/finance/company_news?q=NASDAQ%3A{}&start=0&num=5&startdate={}&enddate={}'.format(code, start, end)
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html5lib')
+    articles = soup.find_all("a", {"id":"n-cn-"}) 
+    link_title = [(x.attrs['href'], x.text) for x in articles]
+    print(link_title)
+    return jsonify({'link_titles': link_title})
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
