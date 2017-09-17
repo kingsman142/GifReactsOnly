512a95afdf738fdd47b0b1a3d97870f98edd63f2
kingsman142 <jamhahn@yahoo.com>
2017-09-17 10:04:53 -0400

    done a lot of work on ui

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Image, Text, View, StyleSheet } from 'react-native';
+import { ScrollView, Button, Image, Text, View, StyleSheet } from 'react-native';
 import { ImagePicker } from 'expo';
 import vision from 'react-cloud-vision-api';
+import Swiper from 'react-native-swiper';
+//import { Button } from 'react-native-button';
 
 vision.init({ auth: 'AIzaSyC7fk12dbsYVi7x4zBC4suE3zQpJQboIGU' })
 
@@ -11,6 +13,8 @@ export default class ImagePickerExample extends React.Component {
 
     this.state = {
      image: null,
+     dates: null,
+     gifs: null
     }
 
     this.captureImage();
@@ -19,73 +23,120 @@ export default class ImagePickerExample extends React.Component {
   state = {
     image: null,
     status: null,
+    dates: null,
+    gifs: null
   };
 
   render() {
-    let { image, status } = this.state;
+    let { image, status, dates, gifs } = this.state;
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#0088cc' }}>
         <Button
           title="Take Another Picture"
           onPress={this.captureImage}
+          color="#e6b800"
         />
         {image &&
           <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
         {status && <Text>{status}</Text>}
+        {<Swiper style={styles.wrapper} showsButtons={true}>
+            <View style={styles.slide1}>
+              <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+            <View style={styles.slide2}>
+              <Text style={styles.text}>Beautiful</Text>
+            </View>
+            <View style={styles.slide3}>
+              <Text style={styles.text}>And simple</Text>
+            </View>
+          </Swiper>}
+        /*{ dates &&
+          dates.map((date) => (
+              <Button title={date} onPress={this.findGiphyNews} key={date} color="#e6b800" />
+          ))
+        }
+        { gifs &&
+           gifs.map((gifUrl) => (
+               <Image
+                  style={{width: 200, height: 200}}
+                  source={{uri: gifUrl}}
+                  key={gifUrl}
+                />
+           ))
+       }*/
       </View>
     );
   }
 
-  captureImage = async () => {
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: false,
-      base64: true,
-    });
-
-    console.log(result);
-
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
-
-      const req = new vision.Request({
-        image: new vision.Image({
-          base64: result.base64,
-        }),
-        features: [
-          //new vision.Feature('TEXT_DETECTION', 1),
-          new vision.Feature('LOGO_DETECTION', 1),
-        ]
-      })
-
-      vision.annotate(req).then((res) => {
-          this.setState({ status: JSON.stringify(res.responses) });
-          console.log(res.responses);
-
-          if (res && res.responses && res.responses[0] && res.responses[0].logoAnnotations && res.responses[0].logoAnnotations[0] && res.responses[0].logoAnnotations[0].description) {
-              return res.responses[0].logoAnnotations[0].description; //Company name
-          }
-          else return '';
-      }, (e) => {
-          let errorText = 'Error: ' + e;
-          console.error(errorText);
-          this.setState({ status: errorText });
-      }).then((companyName) => {
-          console.log('company', companyName);
-          fetch('https://gif-reacts-only.herokuapp.com/getDates', {
-              method: 'POST',
-              timeout: 10000,
-              body: JSON.stringify({'company': companyName})
-          }).then((output) => output.json()).then((parsed) => {
-              let dates = parsed.dates;
-              console.log(dates);
-              this.setState({ dates: dates });
-          }).catch((error) => {
-              console.error(error);
-          });
-      });
+  findGiphyNews = async () => {
+        /*fetch('https://gif-reacts-only.herokuapp.com/getGifs', {
+            method: 'POST',
+            timeout: 10000,
+            body: JSON.stringify({'company': companyName})
+        }).then((output) => output.json()).then((parsed) => {
+            let dates = parsed.dates;
+            console.log(dates);
+            this.setState({ dates: dates });
+        }).catch((error) => {
+            console.error(error);
+        });*/
+        gifs = ["https://media3.giphy.com/media/5xtDarEbygs3Pu7p3jO/200_d.gif", "https://media.giphy.com/media/3o6oziEt5VUgsuunxS/giphy.gif"]
+        this.setState({ gifs: gifs });
     }
-  };
+
+    captureImage = async () => {
+      let result = await ImagePicker.launchCameraAsync({
+        allowsEditing: false,
+        base64: true,
+      });
+
+      console.log(result);
+
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+
+        const req = new vision.Request({
+          image: new vision.Image({
+            base64: result.base64,
+          }),
+          features: [
+            //new vision.Feature('TEXT_DETECTION', 1),
+            new vision.Feature('LOGO_DETECTION', 1),
+          ]
+        })
+
+        vision.annotate(req).then((res) => {
+            console.log(res.responses);
+
+            if (res && res.responses && res.responses[0] && res.responses[0].logoAnnotations && res.responses[0].logoAnnotations[0] && res.responses[0].logoAnnotations[0].description) {
+                this.setState({ status: JSON.stringify(res.responses[0].logoAnnotations[0].description) });
+                return res.responses[0].logoAnnotations[0].description; //Company name
+            }
+            else {
+                this.setState({ status: JSON.stringify("Unidentified Company")});
+                return '';
+            }
+        }, (e) => {
+            let errorText = 'Error: ' + e;
+            console.error(errorText);
+            this.setState({ status: errorText });
+        }).then((companyName) => {
+            console.log('company', companyName);
+            fetch('https://gif-reacts-only.herokuapp.com/getDates', {
+                method: 'POST',
+                timeout: 10000,
+                body: JSON.stringify({'company': companyName})
+            }).then((output) => output.json()).then((parsed) => {
+                let dates = parsed.dates;
+                console.log(dates);
+                this.setState({ dates: dates });
+            }).catch((error) => {
+                console.error(error);
+            });
+        });
+      }
+    };
 }
 
 const styles = StyleSheet.create({
@@ -105,5 +156,29 @@ const styles = StyleSheet.create({
     color: '#000',
     padding: 10,
     margin: 40
+},wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
   }
 });
