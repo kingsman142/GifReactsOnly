a021eb2351b844d3af26a454d2a111381185bb88
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 22:23:00 -0400

    takes post

--- app.py
+++ app.py
@@ -2,16 +2,20 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-    return """
+    if request.method == 'POST':
+        return """
 
-	<h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+        <h1>Hello heroku</h1>
+        <p>It is currently {time}.</p>
 
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
-
+        <img src="http://loremflickr.com/600/400">
+        """.format(time=the_time)
+    else:
+        return '''
+        <h1>no post</h1>
+        '''
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
