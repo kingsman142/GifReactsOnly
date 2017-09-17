d47345069ff7e03e2b959768410d8757eb8a5499
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 23:48:34 -0400

    commenting out for react

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request, url_for, jsonify
 import requests
 import json
 app = Flask(__name__)
+
 def get_stuff(query):
     return requests.get('http://d.yimg.com/aq/autoc?query=' + query + '&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks').text
 
@@ -12,12 +13,12 @@ def get_stuff(query):
 @app.route('/tests/endpoint', methods=['GET', 'POST'])
 def my_test_endpoint():
     
-    input_json = request.get_json(force=True) 
+#    input_json = request.get_json(force=True) 
     # force=True, above, is necessary if another developer 
     # forgot to set the MIME type to 'application/json'
     # print('data from client:', input_json)
-    company = input_json['company']
-    x = get_stuff(company)
+#company = input_json['company']
+    x = get_stuff('google')
     dictToReturn = {'answer': json.loads(x[x.find('{'):-2])}
     return jsonify(dictToReturn)
 
