ff48894b0fc46bf7970e1f3e5f67cb9dc2dd8d7f
kingsman142 <jamhahn@yahoo.com>
2017-09-16 22:07:41 -0400

    made camera working

--- gif-reacts-ui/App.js
+++ gif-reacts-ui/App.js
@@ -1,30 +1,65 @@
+'use strict';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Image, Text, View } from 'react-native';
 import {
   AppRegistry,
   Dimensions,
   TouchableHighlight,
 } from 'react-native';
-import { Camera } from 'react-native-camera';
+import { ImagePicker } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    image: null,
+  };
+
   render() {
+    let { image } = this.state;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Pick an image from camera roll"
+          onPress={this._pickImage}
+        />
+        {image &&
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
+    );
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: false,
+  }, function(){
+      ajax({
+          url: "https://gif-reacts-only.herokuapp.com",
+          type: "POST",
+          body: {
+              image: new File(["foo"], "foo.txt", {
+              }),
+          },
+          success: function(){
+              console.log("submitted image to server");
+          }
+      })
+  });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+  /*render() {
     return (
       <View style={styles.container}>
-          /*<Camera
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-            style={styles.preview}
-            aspect={Camera.constants.Aspect.fill}>
-            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-          </Camera>*/
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
-  }
+    }*/
 }
 
 const styles = StyleSheet.create({
