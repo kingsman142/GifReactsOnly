5c3f62f14f56f990c58935cc703b8c8d50ae71f0
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 23:33:54 -0400

    update

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ import os
 import logging
 from flask import Flask, render_template, request, url_for, jsonify
 #from werkzeug.utils import secure_filename
-
+import requests
+import json
 app = Flask(__name__)
+def get_stuff(query):
+    return requests.get('http://d.yimg.com/aq/autoc?query=' + query + '&region=US&lang=en-US&callback=YAHOO.util.ScriptNodeDataSource.callbacks').text
 
 
-@app.route('/tests/endpoint', methods=['POST'])
+@app.route('/tests/endpoint', methods=['GET', 'POST'])
 def my_test_endpoint():
     
-#input_json = request.get_json(force=True) 
+    input_json = request.get_json(force=False) 
     # force=True, above, is necessary if another developer 
     # forgot to set the MIME type to 'application/json'
 #    print('data from client:', input_json)
-    dictToReturn = {'answer':42}
+    company = input_json['company']
+    x = get_stuff(company).text
+    dictToReturn = {'answer': json.loads(x[x.find('{'):-2])}
     return jsonify(dictToReturn)
 
 if __name__ == '__main__':
