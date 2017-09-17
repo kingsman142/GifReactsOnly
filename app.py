c887bb227899f14d3cc36f2b8ca520588d6c726f
Daniel Zheng <dzheng256@gmail.com>
2017-09-16 23:06:08 -0400

    delete reading input

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 
 @app.route('/tests/endpoint', methods=['POST'])
 def my_test_endpoint():
-    input_json = request.get_json(force=True) 
+    
+#input_json = request.get_json(force=True) 
     # force=True, above, is necessary if another developer 
     # forgot to set the MIME type to 'application/json'
-    print('data from client:', input_json)
+#    print('data from client:', input_json)
     dictToReturn = {'answer':42}
     return jsonify(dictToReturn)
 
